b1c35d402a581677b23d671065b37c0eb78e3e8d
sowmyabhatc <74143897+sowmyabhatc@users.noreply.github.com>
2020-12-02 15:04:25 +0530

    Add files via upload

---
+++ appconstant.h
@@ -0,0 +1,10 @@
+#ifndef APPCONSTANT_H_INCLUDED
+#define APPCONSTANT_H_INCLUDED
+
+#define Q_LEN        32
+#define QUEUE_OK     1
+#define QUEUE_EMPTY  2
+#define QUEUE_FULL   3
+
+
+#endif // APPCONSTANT_H_INCLUDED
